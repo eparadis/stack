3bcdb6d7b943444cf1c644b92ae0d25b1eab25ba
Ed Paradis <legomaniac@gmail.com>
2013-05-24 17:08:59 -0700

    added seperate control stack and loop capability

--- stack.py
+++ stack.py
@@ -1,6 +1,6 @@
 # simple stack based (RPN) calculator
 # originally written in ruby by ERP Nov 2011
-# converted to python ERP May 2013
+# converted to python and extended by ERP May 2013
 
 import re
 
@@ -27,36 +27,73 @@ class StackOperation:
         stack.append( temp)
         stack.append( temp)
 
+    def doUntil( self, data, cmd):
+        temp = int(data.pop() )
+        if temp != 0:
+            # end the loop
+            cmd.pop()
+            return -1
+        else:
+            # go back to the latest 'begin'
+            return cmd.pop()
+
+    def startLoop( self, cmd, index):
+        # mark the beginning of a loop
+        cmd.append( index)
+
+    def doTestZero( self, stack):
+        temp = int( stack.pop() )
+        stack.append( temp) # we leave the tested value on the stack
+        if temp == 0:
+            stack.append( 1)
+        else:
+            stack.append( 0)
 
 so = StackOperation()
 
 # get a string
-user_input = raw_input( '> ') 
+#user_input = raw_input( '> ') 
+user_input = "10 begin 1 - dup . ?0 until"
 
 # split it into an array
 myarray = user_input.split()
 
-stack = []
+dataStack = []
+cmdStack = []
 
 # elements into the array as either integers or strings
-for element in myarray:
+PC = 0
+while PC < len(myarray):
+    #print "DEBUG: PC = ", PC
+    element = myarray[PC]
     if( re.compile(r"^[0123456789]+").search(element) ):
-        stack.append( int(element) )   
+        dataStack.append( int(element) )   
     elif( element == "+" ): 
-        so.doAdd( stack)
+        so.doAdd( dataStack)
     elif( element == "-" ):
-        so.doSubtract( stack)
+        so.doSubtract( dataStack)
     elif( element == "*" ):
-        so.doMultiply( stack)
+        so.doMultiply( dataStack)
     elif( element == "/" ):
-        so.doDivide( stack)
+        so.doDivide( dataStack)
     elif( element == "." ):
-        so.doPop( stack)
+        so.doPop( dataStack)
     elif( element == "dup" ):
-        so.doDuplicate( stack)
+        so.doDuplicate( dataStack)
+    elif( element == 'begin' ):
+        so.startLoop( cmdStack, PC)
+    elif( element == 'until' ):
+        ret = so.doUntil( dataStack, cmdStack)
+        if ret != -1:
+            PC = ret
+            continue
+        # otherwise just increment PC like usual
+    elif( element == '?0' ):
+        so.doTestZero( dataStack)
     else:
         print "Unknown word: " + element
         break
+    PC += 1
 
-print stack
+print dataStack
 
